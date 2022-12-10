d10543548bc93a6aacc4eedb904625d8c448197a
maksPodstawski <mp305396@student.polsl.pl>
2022-12-10 22:55:13 +0100

    commit 4 create people migration

---
+++ database/migrations/2022_12_10_161221_create_people_table.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_num');
+            $table->string('street');
+            $table->string('city');
+            $table->string('country');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
