e8f1d55fe38ce8c813de8afbdd66dc8b1668cf95
maksPodstawski <mp305396@student.polsl.pl>
2022-12-17 17:52:17 +0100

    commit 10 visual corrections in code

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PeoplePostRequest;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use App\Models\People;
 
 class PeopleController extends Controller
--- database/migrations/2022_12_10_161221_create_people_table.php
+++ database/migrations/2022_12_10_161221_create_people_table.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('people', function (Blueprint $table) {
@@ -28,11 +23,6 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('people');
